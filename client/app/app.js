2a15702678b221d1515b441bc507196fff69cad9
Jeaneth Farmer <xaid27@gmail.com>
2015-01-19 10:30:58 -0600

    init app

---
+++ client/app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('pokerestimateApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'btford.socket-io'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
