46f05ad44f3a2ec9a8c9d4559c03bb28c44cf8df
Jeaneth Farmer <xaid27@gmail.com>
2015-01-20 10:36:47 -0600

    added route for session view

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,7 @@
 'use strict';
 
 var errors = require('./components/errors');
+var path = require('path');
 
 module.exports = function(app) {
 
@@ -12,12 +13,26 @@ module.exports = function(app) {
   app.use('/api/things', require('./api/thing'));
 
   // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
+  app.get('/:url(api|auth|components|app|bower_components|assets)/*')
 
-  // All other routes should redirect to the index.html
-  app.route('/*')
-    .get(function(req, res) {
-      res.sendfile(app.get('appPath') + '/index.html');
+  app.get('/*', function(req, res) {
+    var stripped = req.url.split('/')[1];
+    res.render(stripped, function (err, view) {
+      if(err) {
+        console.log("Error rendering partial '" + requestedView + "'\n", err);
+        res.send(404, {error: err});
+      }
+      res.send(view);
     });
+  });
+
+  //app.get('/session', function(req, res) {
+  //  res.render('session', function (err, view) {
+  //    if(err) {
+  //      console.log("Error rendering partial '" + requestedView + "'\n", err);
+  //      res.send(404, {error: err});
+  //    }
+  //    res.send(view);
+  //  });
+  //});
 };
