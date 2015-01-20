3c282810c348ad75f6e074c59140be065b4691b7
Jeaneth Farmer <xaid27@gmail.com>
2015-01-20 15:52:49 -0600

    added route for sessions and set false to html5mode in routeprovider

--- client/app/app.js
+++ client/app/app.js
@@ -5,13 +5,22 @@ angular.module('pokerestimateApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
-  'btford.socket-io'
+  'btford.socket-io',
+  'mm.foundation'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/sessions/:id', {
+        templateUrl: 'app/session/session.html',
+        controller: 'SessionCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
 
-    $locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);
   });
