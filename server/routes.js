8954bf65d5ad138e63deb88b6fd63323a5e64383
Jeaneth Farmer <xaid27@gmail.com>
2015-01-20 15:51:14 -0600

    don't need server routes for now

--- server/routes.js
+++ server/routes.js
@@ -9,30 +9,4 @@ var path = require('path');
 
 module.exports = function(app) {
 
-  // Insert routes below
-  app.use('/api/things', require('./api/thing'));
-
-  // All undefined asset or api routes should return a 404
-  app.get('/:url(api|auth|components|app|bower_components|assets)/*')
-
-  app.get('/*', function(req, res) {
-    var stripped = req.url.split('/')[1];
-    res.render(stripped, function (err, view) {
-      if(err) {
-        console.log("Error rendering partial '" + requestedView + "'\n", err);
-        res.send(404, {error: err});
-      }
-      res.send(view);
-    });
-  });
-
-  //app.get('/session', function(req, res) {
-  //  res.render('session', function (err, view) {
-  //    if(err) {
-  //      console.log("Error rendering partial '" + requestedView + "'\n", err);
-  //      res.send(404, {error: err});
-  //    }
-  //    res.send(view);
-  //  });
-  //});
 };
