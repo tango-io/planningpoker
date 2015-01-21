4256ca9a7800a53cb15caa1fcccbc6ef874ccefe
Jeaneth Farmer <xaid27@gmail.com>
2015-01-21 11:35:17 -0600

    user can vote secretly

--- client/app/session/session.controller.js
+++ client/app/session/session.controller.js
@@ -14,22 +14,21 @@ angular.module('pokerestimateApp')
 
     $scope.socket.on('joinedSession', function(id){
       $scope.id  = id;
-      $scope.currentUser.id = id;
     });
 
     $scope.socket.on('updateUsers', function(data){
       $scope.users = data.users;
+      $scope.currentUser = _.findWhere(data.users, {socketId: $scope.id});
+    });
+
+    $scope.socket.on('descriptionUpdated', function(description){
+      $scope.description = description;
     });
   };
 
-  //$scope.socket.on('descriptionUpdated', function(description){
-  //  $scope.description = description;
-  //});
-
-
-  //$scope.updateDescription = function(){
-  //  $scope.socket.emit('updateDescription', {id: $scope.sessionId, description: $scope.description});
-  //}
+  $scope.updateDescription = function(){
+    $scope.socket.emit('updateDescription', {id: $scope.sessionId, description: $scope.description});
+  };
 
   //$scope.clearValues = function(){
   //  $scope.voteValues = [];
@@ -37,12 +36,9 @@ angular.module('pokerestimateApp')
   //  //$scope.players = _.map($scope.players, function(player){ return _.omit(player, 'vote', 'voted'); });
   //};
 
-  //$scope.setVote = function(vote){
-  //  $scope.vote = vote;
-  //  safeApply($scope, function(){
-  //    //$scope.currentUser.voted = true;
-  //  });
-  //  $scope.votes[$scope.currentUser.username] = vote;
-  //  $scope.socket.emit('vote', {id:$scope.sessionId, user:$scope.currentUser});
-  //};
+  $scope.setVote = function(vote){
+    $scope.currentUser.voted = true;
+    $scope.votes[$scope.id] = vote;
+    //$scope.socket.emit('vote', {id:$scope.sessionId, user:$scope.currentUser.id});
+  };
 });
