11aaa1338522ecf39ea799c123221b74e0e7dbd8
Jeaneth Farmer <xaid27@gmail.com>
2015-01-23 16:45:51 -0600

    trying to add socket tests

--- server/components/sockets/main.spec.js
+++ server/components/sockets/main.spec.js
@@ -1,21 +1,83 @@
 'use strict';
 
 var should = require('should');
-var app = require('../../app');
-var request = require('supertest');
+var io = require('socket.io-client');
+var url = require('../../config/local.env').DOMAIN;
+//var server = require('../../config/socketio');
 
-describe('GET /api/things', function() {
+var options ={
+  transports: ['websocket'],
+  'force new connection': true
+};
 
-  it('should respond with JSON array', function(done) {
-    request(app)
-      .get('/api/things')
-      .expect(200)
-      .expect('Content-Type', /json/)
-      .end(function(err, res) {
-        if (err) return done(err);
-        res.body.should.be.instanceof(Array);
+describe('sockets', function() {
+
+  var server,
+
+  options ={
+    transports: ['websocket'],
+    'force new connection': true
+  };
+
+  beforeEach(function (done) {
+    server = require('../../app').server;
+    done();
+  });
+
+
+  it('emits an event when session is created', function(done) {
+    var client = io.connect(url, options);
+    this.timeout(10000);
+
+    client.on('connect', function(data){
+        console.log("________________", sessionID);
+      client.emit('newSession')
+      done();
+    });
+
+      client.on('newSession', function(sessionID){
+        console.log("________________", sessionID);
+        client1.disconnect();
         done();
       });
+    //client.on('sessionCreated',function(sessionId){
+    //  //usersName.should.be.type('string');
+    //  //usersName.should.equal(chatUser1.name + " has joined.");
+    //  console.log("aklsjdalsd", sessionId);
+    //  client.disconnect();
+    //});
+    //client1.send('newSession');
   });
+
+  //it('emits update users when a user join in a session', function(done) {
+      //var client1 = io.connect(url);
+
+      //client1.send('newSession', client1);
+      //client2.on('connect', function(data){
+      //  client2.emit('connection name', chatUser2);
+      //});
+
+  //});
+
+  //it('emits hide votes when user joins in a session', function(done) {
+  //});
+
+  //it('emits an error event when session does not exist', function(done) {
+  //});
+
+  //it('updates description for all clients', function(done) {
+  //});
+
+  //it('updates votes for all clients', function(done) {
+  //});
+
+  //it('emits to reveal votes when all clients has voted', function(done) {
+  //});
+
+  //it('emits clear votes after clearing a session', function(done) {
+  //});
+
+  //it('updates users and votes after a client leaves', function(done) {
+  //});
 });
 
