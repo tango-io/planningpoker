a70607639d099da4db126e75a0641ee46380256e
Jeaneth Farmer <xaid27@gmail.com>
2015-01-26 10:07:08 -0600

    remove mongoose

--- server/app.js
+++ server/app.js
@@ -8,11 +8,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 var config = require('./config/environment');
 
 // Connect to database
-mongoose.connect(config.mongo.uri, config.mongo.options);
+//mongoose.connect(config.mongo.uri, config.mongo.options);
 
 // Populate DB with sample data
 if(config.seedDB) { require('./config/seed'); }
