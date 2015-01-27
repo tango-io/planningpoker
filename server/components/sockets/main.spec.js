5b2cf4560bf5059c0c5d46c3131c394fd0168eb0
Jeaneth Farmer <xaid27@gmail.com>
2015-01-27 12:19:10 -0600

    6 server tests passing

--- server/components/sockets/main.spec.js
+++ server/components/sockets/main.spec.js
@@ -2,8 +2,8 @@
 
 var should = require('should');
 var io = require('socket.io-client');
+var _ = require('lodash');
 var url = require('../../config/local.env').DOMAIN;
-//var server = require('../../config/socketio');
 
 var options ={
   transports: ['websocket'],
@@ -12,72 +12,208 @@ var options ={
 
 describe('sockets', function() {
 
-  var server,
+  var server, client1, client2;
 
-  options ={
+  var options = {
     transports: ['websocket'],
-    'force new connection': true
+    'force new connection': true,
     path: '/socket.io-client'
   };
 
+  server = require('../../app').server;
+
   beforeEach(function (done) {
-    server = require('../../app').server;
+    client1 = io.connect(url, options);
+    client2 = io.connect(url, options);
+
+    client1.on('connect', function(data){
+      client2.on('connect', function(data){
+        done();
+      });
+    });
+  });
+
+  afterEach(function (done) {
+    client1.disconnect();
+    client2.disconnect();
     done();
   });
 
-
   it('emits an event when session is created', function(done) {
-    var client = io.connect(url, options);
-
-    client.on('connect', function(data){
-        console.log("________________", sessionID);
-      client.emit('newSession')
+    client1.on('sessionCreated', function(sessionId){
+      sessionId.should.match(/^.{8}-.{4}-.{4}-.{4}-.{12}/)
       done();
     });
 
-      client.on('newSession', function(sessionID){
-        console.log("________________", sessionID);
-        client1.disconnect();
-        done();
-      });
-    //client.on('sessionCreated',function(sessionId){
-    //  //usersName.should.be.type('string');
-    //  //usersName.should.equal(chatUser1.name + " has joined.");
-    //  console.log("aklsjdalsd", sessionId);
-    //  client.disconnect();
-    //});
-    //client1.send('newSession');
+    client1.emit('newSession');
   });
 
-  //it('emits update users when a user join in a session', function(done) {
-      //var client1 = io.connect(url);
+  it('emits an error event when session does not exist', function(done) {
+    client1.emit('joinSession', {id:'not-existing-id'});
 
-      //client1.send('newSession', client1);
-      //client2.on('connect', function(data){
-      //  client2.emit('connection name', chatUser2);
-      //});
+    client1.on('errorMsg', function(data){
+      data.message.should.be.exactly("Session does not exist")
+      done();
+    });
+  });
 
-  //});
+  it('emits hide votes when user joins in a session', function(done) {
+    client1.emit('newSession');
+    client1.on('sessionCreated', function(id){
 
-  //it('emits hide votes when user joins in a session', function(done) {
-  //});
+      client1.emit('joinSession', {id: id, username: 'Tester'});
 
-  //it('emits an error event when session does not exist', function(done) {
-  //});
+      client1.on('updateUsers', function(){
+        client2.emit('joinSession', {id: id, username: 'Another tester'});
 
-  //it('updates description for all clients', function(done) {
-  //});
+        client1.on('hideVotes', function(data){
+          done();
+        });
+      });
+    });
+  });
 
-  //it('updates votes for all clients', function(done) {
-  //});
+  it('emits update users when a user join in a session', function(done) {
+    client1.emit('newSession');
+    client1.on('sessionCreated', function(id){
+
+      client1.emit('joinSession', {id: id, username: 'Tester'});
+
+      client1.on('updateUsers', function(data){
+
+        client2.emit('joinSession', {id: id, username: 'Another tester'});
+
+        client1.on('updateUsers', function(data){
+          data.users[0].username.should.be.exactly('Tester');
+          data.users[1].username.should.be.exactly('Another tester');
+          done();
+        })
+      });
+    });
+  });
+
+  it('updates description for all clients', function(done) {
+    client1.emit('newSession');
+
+    client1.on('sessionCreated', function(id){
+
+      client1.emit('joinSession', {id: id, username: 'Tester'});
+      client2.emit('joinSession', {id: id, username: 'Another tester'});
+
+      client1.emit('updateDescription', {id: id, description: 'Hello'});
+      client2.on('descriptionUpdated', function(data){
+        data.should.be.exactly('Hello');
+        done();
+      })
+    });
+  });
+
+  it('updates vote flag when a user votes', function(done) {
+    client1.emit('newSession');
+    client1.on('sessionCreated', function(id){
+      client1.emit('joinSession', {id: id, username: 'Tester'});
+
+      client1.once('updateUsers', function(data){
+        client2.emit('joinSession', {id: id, username: 'Another tester'});
+
+        client2.once('updateUsers', function(data){
+
+          client1.emit('vote', {id: id, userId: client1.id, vote: 3 });
+
+          client2.once('updateUsers', function(data){
+            data.users[0].voted.should.be.ok;
+            done();
+          });
+        });
+      });
+    });
+  });
 
   //it('emits to reveal votes when all clients has voted', function(done) {
+  //  client1.emit('newSession');
+
+  //  client1.on('sessionCreated', function(id){
+
+  //    client1.emit('joinSession', {id: id, username: 'Tester'});
+  //    client2.emit('joinSession', {id: id, username: 'Another tester'});
+
+  //    client1.emit('vote', {id: id, userId: client1.id, vote: 4 });
+  //    client2.emit('vote', {id: id, userId: client2.id, vote: 5 });
+
+  //    client2.on('updateVotes', function(data){
+  //      data[client1.id].should.be.exactly(4);
+  //      data[client2.id].should.be.exactly(5);
+  //      done();
+  //    })
+  //  });
   //});
 
   //it('emits clear votes after clearing a session', function(done) {
+  //  client1.emit('newSession');
+  //  client1.on('sessionCreated', function(id){
+
+  //    client1.emit('joinSession', {id: id, username: 'Tester'});
+  //    client2.emit('joinSession', {id: id, username: 'Another tester'});
+
+  //    client1.emit('clearSession', {id: id});
+
+  //    client2.on('clearVotes', function(data){
+  //      done();
+  //    })
+  //  });
   //});
 
-  //it('updates users and votes after a client leaves', function(done) {
+  //function startSessions(cb){
+  //  client1.emit('newSession');
+  //  client1.on('sessionCreated', function(id){
+
+  //    client1.emit('joinSession', {id: id, username: 'Tester'});
+
+  //    client1.on('updateUsers', function(data){
+  //      client2.emit('joinSession', {id: id, username: 'Another tester'});
+  //      client1.on('updateUsers', function(data){
+  //        return cb();
+  //      });
+  //    });
+  //  };
+
+  //it('updates users after a client leaves', function(done) {
+  //  client1.emit('newSession');
+  //  client1.on('sessionCreated', function(id){
+
+  //    client1.emit('joinSession', {id: id, username: 'Tester'});
+
+  //    client1.on('updateUsers', function(data){
+  //      client2.emit('joinSession', {id: id, username: 'Another tester'});
+  //      client1.on('updateUsers', function(data){
+  //        client2.emit('leaveSession', {id: id, username: 'Another tester'});
+
+  //        client1.on('updateUsers', function(data){
+  //          console.log("____________", data);
+  //          data.users.length.should.be.exactly(1);
+  //          done();
+  //        });
+  //      });
+  //    });
+  //  });
+  //});
+
+  //it('updates votes after a client leaves', function(done) {
+  //  client1.emit('newSession');
+  //  client1.on('sessionCreated', function(id){
+
+  //    client1.emit('joinSession', {id: id, username: 'Tester'});
+  //    client2.emit('joinSession', {id: id, username: 'Another tester'});
+
+  //    client2.emit('vote', {id: id, userId: client2.id, vote: 5 });
+
+  //    client2.emit('leaveSession', {id: id, username: 'Another tester'});
+
+  //    client1.on('updateVotes', function(data){
+  //      var votesLength = _.keys(data).length
+  //      votesLength.should.be.exactly(0);
+  //      done();
+  //    });
+  //  });
   //});
 });
-
