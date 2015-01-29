5fd0752cf2c31535072fde1effc1fab1a36473c5
Jeaneth Farmer <xaid27@gmail.com>
2015-01-29 15:38:07 -0600

    able to remove vote

--- client/app/voteValues/voteValues.controller.js
+++ client/app/voteValues/voteValues.controller.js
@@ -1,17 +1,25 @@
 'use strict';
 
 angular.module('pokerestimateApp')
-  .controller('VoteValuesCtrl', function ($scope) {
-    //$location.path('/sessions/' + $scope.sessionId);
+  .controller('VoteValuesCtrl', function ($scope, $routeParams, userService, $location) {
     $scope.init = function(){
-      $scope.voteValues = {
-        0: 0,
-        1: 1,
-        2: 2,
-        3: 3,
-        5: 4,
-        8: 5,
-        13: 13
-      };
-    }
+      $scope.voteValues = [
+        {label: 0, value: 0},
+        {label: 1, value: 1},
+        {label: 2, value: 2},
+        {label: 3, value: 3},
+        {label: 5, value: 5},
+        {label: 8, value: 8},
+        {label: 13, value: 13}
+      ];
+    };
+
+    $scope.go = function(){
+     userService.setVoteValues($scope.voteValues);
+     $location.path('/sessions/' + $routeParams.id);
+    };
+
+    $scope.removeValue = function(value){
+      $scope.voteValues = _.reject($scope.voteValues, {value: value});
+    };
   });
