766bda10975295e71da63b05791cc044413d3c33
Jeaneth Farmer <xaid27@gmail.com>
2015-01-30 12:55:13 -0600

    added e2e test for vote values page

---
+++ e2e/voteValues/voteValues.po.js
@@ -0,0 +1,39 @@
+/**
+ * This file uses the Page Object pattern to define the main page for tests
+ * https://docs.google.com/presentation/d/1B6manhG0zEXkC-H-tPo2vwU06JhL8w9-XCF9oehXzAQ
+ */
+
+'use strict';
+
+var MainPage = function() {
+  this.container = element(by.css('.main-content'));
+  this.form = this.container.element(by.css('form[name="startSessionForm"]'));
+
+  this.usernameInput = this.container.element(by.model('username'));
+  this.startBtn = this.form.element(by.css('button'));
+
+  //this.voteListRow = element(by.repeater('vote in voteValues').row(0).column('vote.value'));
+  this.voteList = element.all(by.repeater('vote in voteValues'));
+  this.voteListFirstLabel = element(by.css('tr:first-child td:first-child input'));
+  this.voteListFirstValue = element(by.css('tr:first-child td:nth-child(2) input'));
+  this.voteListFirstBtn = element(by.css('tr:first-child td:last-child button'));
+
+  this.voteListFirst = element(by.css('ul.button-group li:first-child button'));
+  this.votesFirst = element(by.css('tr[ng-repeat="user in users"] td:last-child'));
+
+  this.usernameInput_ = this.container.element(by.model('username_'));
+  this.sessionIdInput = this.container.element(by.model('sessionId'));
+  this.joinBtn = this.container.element(by.css('form[name="joinSessionForm"] button'));
+
+  this.clearBtn = element(by.buttonText('Clear Votes'));
+  this.showBtn = element(by.buttonText('Show Votes'));
+  this.goBtn = element(by.buttonText('Go ->'));
+
+  this.modal = element(by.css('.reveal-modal'));
+  this.usernameModalInput = this.modal.element(by.model('username'));
+  this.modalBtn = this.modal.element(by.buttonText('OK'));
+};
+
+module.exports = new MainPage();
+
+
