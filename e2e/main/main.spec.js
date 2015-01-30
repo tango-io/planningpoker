2819212d9bf8aa09f69d99b26bae9e438c822871
Jeaneth Farmer <xaid27@gmail.com>
2015-01-30 10:02:29 -0600

    update main spec for new behaviour, really testing join a session

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -11,13 +11,13 @@ describe('Main View', function() {
     browser.waitForAngular();
   });
 
-  it('s able to create a session', function() {
+  it('s able to go to vote values page', function() {
     page.usernameInput.sendKeys('Arya');
     page.startBtn.click();
-    expect(browser.getCurrentUrl()).toMatch('#/sessions/');
+    expect(browser.getCurrentUrl()).toMatch('#/voteValues');
   });
 
-  it('s not able to create a session without a username', function() {
+  it('s not able to go vote values page without a username', function() {
     page.startBtn.click();
     expect(page.usernameError.getText()).toBe('Please enter a username');
   });
@@ -26,29 +26,41 @@ describe('Main View', function() {
     var id;
     page.usernameInput.sendKeys('Arya');
     page.startBtn.click();
+    page.goBtn.click();
 
     browser.getCurrentUrl().then(function(url){
-      id = url.split('/')[4];
-      browser.get('/');
-      page.usernameInput_.sendKeys('Cersei');
-      page.sessionIdInput.sendKeys(id);
-      page.joinBtn.click();
-      browser.getCurrentUrl().then(function(path){
-        expect(path).toMatch('#/sessions/' + id);
-      });
-    })
+      id = url.split('/')[5];
+      expect(id).toMatch(/^.{8}-.{4}-.{4}-.{4}-.{12}/);
 
+      browser.driver.executeScript('window.open();');
+      browser.getAllWindowHandles().then(function (handles) {
+        var newWindowHandle = handles[1];
+        browser.switchTo(newWindowHandle).window(newWindowHandle).then(function () {
+          browser.driver.executeScript('window.focus();');
+          browser.get('/');
+          page.usernameInput_.sendKeys('Cersei');
+          page.sessionIdInput.sendKeys(id);
+          page.joinBtn.click();
+
+          browser.getCurrentUrl().then(function(path){
+            expect(path).toBe(config.DOMAIN +'/#/sessions/' + id);
+            expect(page.modal.isPresent()).toBe(false);
+          });
+        });
+      });
+    });
   });
 
   it('s not able to join a session that does not exist', function() {
     page.usernameInput_.sendKeys('Sansa');
     page.sessionIdInput.sendKeys('X123');
     page.joinBtn.click();
-    expect(browser.getCurrentUrl()).toMatch('#/sessions/');
-    browser.sleep(2000);
-    expect(page.modal.getText()).toBe("Session does not exist")
-    page.modalBtn.click();
-    expect(browser.getCurrentUrl()).toBe(config.DOMAIN + "/#/");
+    browser.getCurrentUrl().then(function(path){
+      expect(path).toMatch('#/sessions/');
+      expect(page.modal.getText()).toBe("Session does not exist")
+      page.modalBtn.click();
+      expect(browser.getCurrentUrl()).toBe(config.DOMAIN + "/#/");
+    });
   });
 
   it('s not able to join a session without username and session id', function() {
