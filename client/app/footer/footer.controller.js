cf90322abadc6f90f0bf85ff70ed27427c9e4dc0
Jeaneth Farmer <xaid27@gmail.com>
2015-02-05 12:45:11 -0600

    added share functionality for fb and linkedin

--- client/app/footer/footer.controller.js
+++ client/app/footer/footer.controller.js
@@ -5,21 +5,26 @@ angular.module('pokerestimateApp')
   $scope.share = function($event, social){
     $event.preventDefault();
 
-    var width = 500;
+    var description = encodeURIComponent('Best app to point in your sprint planning sessions');
+    var text        = encodeURIComponent('Check this awesome planning poker app');
+    var encodedPath = encodeURIComponent($event.currentTarget.href);
+    var path        = $event.currentTarget.href;
+    var url;
+
+    if(social=='facebook'){
+      return FB.ui({ method: 'share', href: path}, function(response){});
+    }
+
+    if(social == 'twitter'){
+      url = 'http://twitter.com/share?url='+ path +'&text=' + text;
+    }else{
+      url = "http://www.linkedin.com/shareArticle?mini=true&url="+encodedPath+"&title="+text+"&summary="+description+"&source="+encodedPath;
+    }
+
+    var width  = 500;
     var height = 450;
     var left   = ($(window).width()  - width)  / 2;
     var top    = ($(window).height() - height) / 2;
-    var text = encodeURIComponent('Check this awesome planning poker app');
-    var description = encodeURIComponent('Best app to point in your sprint planning sessions');
-    var path = $event.currentTarget.href;
-    var url;
-    if(social == 'twitter'){
-      url = 'http://twitter.com/share?text=' + text;
-    }else if(social=='facebook'){
-      url = 'http://www.facebook.com/sharer/sharer.php?s=100&amp;p[url]='+path+'&amp;p[title]='+text+'[summary]='+text;
-    }else{
-      url = "http://www.linkedin.com/shareArticle?mini=true&url="+encodeURIComponent(path)+"&title="+text+"&summary="+description+"&source="+encodeURIComponent(path);
-    }
 
     $window.open(url, social,'width='+width+', height='+height+', left='+left +', top='+top);
   };
