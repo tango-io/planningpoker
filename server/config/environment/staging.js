82e2f65b0005d677fbf0313e1e1f9564390dc5d0
Jeaneth Farmer <xaid27@gmail.com>
2015-02-05 13:10:50 -0600

    added staging conf

---
+++ server/config/environment/staging.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Staging specific configuration
+// =================================
+module.exports = {
+  // Server IP
+  ip:       process.env.OPENSHIFT_NODEJS_IP ||
+            process.env.IP ||
+            undefined,
+
+  // Server port
+  port:     process.env.OPENSHIFT_NODEJS_PORT ||
+            process.env.PORT ||
+            8080
+};
