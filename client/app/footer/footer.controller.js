27c77396094c3384e2a6f1b8fdd4afcd802825b8
Jeaneth Farmer <xaid27@gmail.com>
2015-02-05 11:14:37 -0600

    added behaviour to share in social

--- client/app/footer/footer.controller.js
+++ client/app/footer/footer.controller.js
@@ -2,13 +2,25 @@
 
 angular.module('pokerestimateApp')
 .controller('FooterCtrl', function ($scope, $window) {
-  $scope.share = function($event){
+  $scope.share = function($event, social){
     $event.preventDefault();
+
     var width = 500;
     var height = 450;
     var left   = ($(window).width()  - width)  / 2;
     var top    = ($(window).height() - height) / 2;
     var text = encodeURIComponent('Check this awesome planning poker app');
-    $window.open('http://twitter.com/share?text=' + text, 'face',' width='+width+', height='+height+', left='+left +', top='+top);
+    var description = encodeURIComponent('Best app to point in your sprint planning sessions');
+    var path = $event.currentTarget.href;
+    var url;
+    if(social == 'twitter'){
+      url = 'http://twitter.com/share?text=' + text;
+    }else if(social=='facebook'){
+      url = 'http://www.facebook.com/sharer/sharer.php?s=100&amp;p[url]='+path+'&amp;p[title]='+text+'[summary]='+text;
+    }else{
+      url = "http://www.linkedin.com/shareArticle?mini=true&url="+encodeURIComponent(path)+"&title="+text+"&summary="+description+"&source="+encodeURIComponent(path);
+    }
+
+    $window.open(url, social,'width='+width+', height='+height+', left='+left +', top='+top);
   };
 });
