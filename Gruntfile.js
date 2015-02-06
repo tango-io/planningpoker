c859c97f3f4f81cb83b51d8e034670c3883d5295
Jeaneth Farmer <xaid27@gmail.com>
2015-02-05 21:47:08 -0600

    update configuration for injector and wiredep

--- Gruntfile.js
+++ Gruntfile.js
@@ -227,7 +227,7 @@ module.exports = function (grunt) {
     // Automatically inject Bower components into the app
     wiredep: {
       target: {
-        src: '<%= yeoman.client %>/index.html',
+        src: '<%= yeoman.client %>/index.jade',
         ignorePath: '<%= yeoman.client %>/',
         exclude: [/bootstrap-sass-official/, /bootstrap.js/, '/json3/', '/es5-shim/', /bootstrap.css/, /font-awesome.css/ ]
       }
@@ -361,7 +361,7 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
-            'index.html'
+            'index.jade'
           ]
         }, {
           expand: true,
@@ -513,19 +513,19 @@ module.exports = function (grunt) {
       options: {
 
       },
-      // Inject application script files into index.html (doesn't include bower)
+      // Inject application script files into index.jade (doesn't include bower)
       scripts: {
         options: {
           transform: function(filePath) {
             filePath = filePath.replace('/client/', '');
             filePath = filePath.replace('/.tmp/', '');
-            return '<script src="' + filePath + '"></script>';
+            return 'script(src="' + filePath + '")';
           },
-          starttag: '<!-- injector:js -->',
-          endtag: '<!-- endinjector -->'
+          starttag: '// injector:js',
+          endtag: '// endinjector'
         },
         files: {
-          '<%= yeoman.client %>/index.html': [
+          '<%= yeoman.client %>/index.jade': [
               ['{.tmp,<%= yeoman.client %>}/{app,components}/**/*.js',
                '!{.tmp,<%= yeoman.client %>}/app/app.js',
                '!{.tmp,<%= yeoman.client %>}/{app,components}/**/*.spec.js',
@@ -553,19 +553,19 @@ module.exports = function (grunt) {
         }
       },
 
-      // Inject component css into index.html
+      // Inject component css into index.jade
       css: {
         options: {
           transform: function(filePath) {
             filePath = filePath.replace('/client/', '');
             filePath = filePath.replace('/.tmp/', '');
-            return '<link rel="stylesheet" href="' + filePath + '">';
+            return 'link(rel="stylesheet" href="' + filePath + '")';
           },
-          starttag: '<!-- injector:css -->',
-          endtag: '<!-- endinjector -->'
+          starttag: '// injector:css',
+          endtag: '// endinjector'
         },
         files: {
-          '<%= yeoman.client %>/index.html': [
+          '<%= yeoman.client %>/index.jade': [
             '<%= yeoman.client %>/{app,components}/**/*.css'
           ]
         }
