70ef88f5116fb498c3b356b016d107f37bf6c383
Jeaneth Farmer <xaid27@gmail.com>
2015-02-10 16:22:21 -0600

    updated e2e tests

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -11,117 +11,121 @@ describe('Main View', function() {
     browser.waitForAngular();
   });
 
-  it('s able to go to vote values page', function() {
-    page.usernameInput.sendKeys('Arya');
-    page.startBtn.click();
-    expect(browser.getCurrentUrl()).toMatch('#/voteValues');
+  describe('Start a session', function() {
+    it('s able to start a session as player', function() {
+      page.usernameInput.sendKeys('Arya');
+      page.playerOpt.click();
+      page.startBtn.click();
+      expect(browser.getCurrentUrl()).toMatch('#/voteValues');
+    });
+
+    it('s able to start a session as moderator', function() {
+      page.usernameInput.sendKeys('Arya');
+      page.moderatorOpt.click();
+      page.startBtn.click();
+      expect(browser.getCurrentUrl()).toMatch('#/voteValues');
+    });
+
+    it('s able to go to vote values page', function() {
+      page.usernameInput.sendKeys('Arya');
+      page.startBtn.click();
+      expect(browser.getCurrentUrl()).toMatch('#/voteValues');
+    });
+
+    it('s not able to go vote values page without a username', function() {
+      page.startBtn.click();
+      expect(page.usernameError.getText()).toBe('Please enter a username');
+    });
   });
 
-  it('s not able to go vote values page without a username', function() {
-    page.startBtn.click();
-    expect(page.usernameError.getText()).toBe('Please enter a username');
-  });
+  describe('Join a session', function() {
+    it('s able to join a session as moderator', function() {
+      var id;
+      page.usernameInput.sendKeys('Arya');
+      page.startBtn.click();
+      page.goBtn.click();
 
-  it('s able to start a session as player', function() {
-    page.usernameInput.sendKeys('Arya');
-    element(by.cssContainingText('.start option', 'Player')).click();
-    page.startBtn.click();
-    expect(browser.getCurrentUrl()).toMatch('#/voteValues');
-  });
+      browser.getCurrentUrl().then(function(url){
+        id = url.split('/')[5];
+        expect(id).toMatch(/^.{8}-.{4}-.{4}-.{4}-.{12}/);
 
-  it('s able to start a session as observer', function() {
-    page.usernameInput.sendKeys('Arya');
-    element(by.cssContainingText('.start option', 'Observer')).click();
-    page.startBtn.click();
-    expect(browser.getCurrentUrl()).toMatch('#/voteValues');
-  });
+        browser.driver.executeScript('window.open();');
+        var appWindow = browser.getWindowHandle();
+        browser.getAllWindowHandles().then(function (handles) {
+          var newWindowHandle = handles[1];
+          browser.switchTo(newWindowHandle).window(newWindowHandle).then(function () {
+            browser.driver.executeScript('window.focus();');
+            browser.get('/');
+            page.usernameInput_.sendKeys('Cersei');
+            page.joinModeratorOpt.click();
+            page.sessionIdInput.sendKeys(id);
+            page.joinBtn.click();
 
-  it('s able to join a session as observer', function() {
-    var id;
-    page.usernameInput.sendKeys('Arya');
-    page.startBtn.click();
-    page.goBtn.click();
-
-    browser.getCurrentUrl().then(function(url){
-      id = url.split('/')[5];
-      expect(id).toMatch(/^.{8}-.{4}-.{4}-.{4}-.{12}/);
-
-      browser.driver.executeScript('window.open();');
-      var appWindow = browser.getWindowHandle();
-      browser.getAllWindowHandles().then(function (handles) {
-        var newWindowHandle = handles[1];
-        browser.switchTo(newWindowHandle).window(newWindowHandle).then(function () {
-          browser.driver.executeScript('window.focus();');
-          browser.get('/');
-          page.usernameInput_.sendKeys('Cersei');
-          element(by.cssContainingText('.join option', 'Observer')).click();
-          page.sessionIdInput.sendKeys(id);
-          page.joinBtn.click();
-
-          browser.getCurrentUrl().then(function(path){
-            expect(path).toBe(config.DOMAIN +'/#/sessions/' + id);
-            expect(page.playersList.count()).toBe(1);
-            expect(page.observersList.count()).toBe(1);
-            expect(page.modal.isPresent()).toBe(false);
-            browser.driver.close().then(function () {
-              browser.switchTo().window(appWindow);
+            browser.getCurrentUrl().then(function(path){
+              expect(path).toBe(config.DOMAIN +'/#/sessions/' + id);
+              expect(page.playersList.count()).toBe(1);
+              expect(page.moderatorsList.count()).toBe(1);
+              expect(page.modal.isPresent()).toBe(false);
+              browser.driver.close().then(function () {
+                browser.switchTo().window(appWindow);
+              });
             });
           });
         });
       });
     });
-  });
 
-  it('s able to join a session as player', function() {
-    var id;
-    page.usernameInput.sendKeys('Arya');
-    page.startBtn.click();
-    page.goBtn.click();
+    it('s able to join a session as player', function() {
+      var id;
+      page.usernameInput.sendKeys('Arya');
+      page.startBtn.click();
+      page.goBtn.click();
 
-    browser.getCurrentUrl().then(function(url){
-      id = url.split('/')[5];
-      expect(id).toMatch(/^.{8}-.{4}-.{4}-.{4}-.{12}/);
+      browser.getCurrentUrl().then(function(url){
+        id = url.split('/')[5];
+        expect(id).toMatch(/^.{8}-.{4}-.{4}-.{4}-.{12}/);
 
-      browser.driver.executeScript('window.open();');
-      var appWindow = browser.getWindowHandle();
-      browser.getAllWindowHandles().then(function (handles) {
-        var newWindowHandle = handles[1];
-        browser.switchTo(newWindowHandle).window(newWindowHandle).then(function () {
-          browser.driver.executeScript('window.focus();');
-          browser.get('/');
-          page.usernameInput_.sendKeys('Cersei');
-          page.sessionIdInput.sendKeys(id);
-          page.joinBtn.click();
+        browser.driver.executeScript('window.open();');
+        var appWindow = browser.getWindowHandle();
+        browser.getAllWindowHandles().then(function (handles) {
+          var newWindowHandle = handles[1];
+          browser.switchTo(newWindowHandle).window(newWindowHandle).then(function () {
+            browser.driver.executeScript('window.focus();');
+            browser.get('/');
+            page.usernameInput_.sendKeys('Cersei');
+            page.sessionIdInput.sendKeys(id);
+            page.joinBtn.click();
 
-          browser.getCurrentUrl().then(function(path){
-            expect(path).toBe(config.DOMAIN +'/#/sessions/' + id);
-            expect(page.playersList.count()).toBe(2);
-            expect(page.observersList.count()).toBe(0);
-            expect(page.modal.isPresent()).toBe(false);
-            browser.driver.close().then(function () {
-              browser.switchTo().window(appWindow);
+            browser.getCurrentUrl().then(function(path){
+              expect(path).toBe(config.DOMAIN +'/#/sessions/' + id);
+              expect(page.playersList.count()).toBe(2);
+              expect(page.moderatorsList.count()).toBe(0);
+              expect(page.modal.isPresent()).toBe(false);
+              browser.driver.close().then(function () {
+                browser.switchTo().window(appWindow);
+              });
             });
           });
         });
       });
     });
-  });
 
-  it('s not able to join a session that does not exist', function() {
-    page.usernameInput_.sendKeys('Sansa');
-    page.sessionIdInput.sendKeys('X123');
-    page.joinBtn.click();
-    browser.getCurrentUrl().then(function(path){
-      expect(path).toMatch('#/sessions/');
-      expect(page.modal.getText()).toBe("Session does not exist")
-      page.modalBtn.click();
-      expect(browser.getCurrentUrl()).toBe(config.DOMAIN + "/#/");
+    it('s not able to join a session that does not exist', function() {
+      page.usernameInput_.sendKeys('Sansa');
+      page.sessionIdInput.sendKeys('X123');
+      page.joinBtn.click();
+      browser.getCurrentUrl().then(function(path){
+        expect(path).toMatch('#/sessions/');
+        expect(page.modal.getText()).toBe("Session does not exist")
+        page.modalBtn.click();
+        expect(browser.getCurrentUrl()).toBe(config.DOMAIN + "/#/");
+      });
     });
-  });
 
-  it('s not able to join a session without username and session id', function() {
-    page.joinBtn.click();
-    expect(page.usernameError_.getText()).toBe('Please enter a username');
-    expect(page.sessionError.getText()).toBe('Please enter a session id');
+    it('s not able to join a session without username and session id', function() {
+      page.joinBtn.click();
+      expect(page.usernameError_.getText()).toBe('Please enter a username');
+      expect(page.sessionError.getText()).toBe('Please enter a session id');
+    });
   });
 });
