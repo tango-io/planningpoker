02d3c0459d51bff88cb70d1000b27e9ef14de8d3
Jeaneth Farmer <xaid27@gmail.com>
2015-02-12 13:28:19 -0600

    fix hide and show behaviours

--- client/app/session/session.controller.js
+++ client/app/session/session.controller.js
@@ -3,21 +3,22 @@
 angular.module('pokerestimateApp')
 .controller('SessionCtrl', function (socket, $scope, $location, $routeParams, $modal, userService) {
   $scope.init = function(){
+    $scope.url         = $location.$$absUrl;// Url to share with the team
     $scope.voteValues  = userService.getVoteValues(); //get default points
     $scope.currentUser = userService.getUser(); //get user name and type
     $scope.sessionId   = $routeParams.id;
     $scope.votes       = {};
 
-    if($scope.currentUser.username){
-     $scope.currentUser.roomId = $scope.sessionId;
-     socket.emit('joinSession', $scope.currentUser);
+    if(userService.getUser().username){
+      $scope.currentUser.roomId = $scope.sessionId;
+      socket.emit('joinSession', $scope.currentUser);
     }else{
       //open modal to ask for username and type when user joins
-      $scope.userType = "player";//default option in modal
+      $scope.userType = "player"; //default option in modal
       var modalInstance = $modal.open({templateUrl: 'app/templates/modals/username.html', keyboard:false, scope: this});
       modalInstance.result.then(function (data) {
-        $scope.currentUser = {username: data.username, type: data.type};
-        socket.emit('joinSession', {username: $scope.currentUser.username, id: $scope.sessionId, type: data.type});
+        $scope.currentUser = {username: data.username, type: data.userType};
+        socket.emit('joinSession', {roomId: $scope.sessionId, username: data.username, type: data.userType});
       });
     }
 
