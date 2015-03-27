f72b0c6164ce7bd956bb91fa9891e44fbf6567b9
Jonatan Juarez <jonatan.ijp@gmail.com>
2015-03-27 10:59:03 -0600

    Added CodeClimate badge

---
+++ test/coverage/instrument/server/config/environment/development.js
@@ -0,0 +1,9 @@
+"use strict";
+var __cov_WkIBrU_2PZ92PQIMc7N7uA = (Function('return this'))();
+if (!__cov_WkIBrU_2PZ92PQIMc7N7uA.__coverage__) { __cov_WkIBrU_2PZ92PQIMc7N7uA.__coverage__ = {}; }
+__cov_WkIBrU_2PZ92PQIMc7N7uA = __cov_WkIBrU_2PZ92PQIMc7N7uA.__coverage__;
+if (!(__cov_WkIBrU_2PZ92PQIMc7N7uA['server/config/environment/development.js'])) {
+   __cov_WkIBrU_2PZ92PQIMc7N7uA['server/config/environment/development.js'] = {"path":"server/config/environment/development.js","s":{"1":0},"b":{},"f":{},"fnMap":{},"statementMap":{"1":{"start":{"line":5,"column":0},"end":{"line":7,"column":2}}},"branchMap":{}};
+}
+__cov_WkIBrU_2PZ92PQIMc7N7uA = __cov_WkIBrU_2PZ92PQIMc7N7uA['server/config/environment/development.js'];
+__cov_WkIBrU_2PZ92PQIMc7N7uA.s['1']++;module.exports={FACEBOOK_APP_ID:'test'};
---
+++ test/coverage/instrument/server/config/local.env.sample.js
@@ -0,0 +1,9 @@
+"use strict";
+var __cov_$gxSu0pudM2bOKAjHXd1vw = (Function('return this'))();
+if (!__cov_$gxSu0pudM2bOKAjHXd1vw.__coverage__) { __cov_$gxSu0pudM2bOKAjHXd1vw.__coverage__ = {}; }
+__cov_$gxSu0pudM2bOKAjHXd1vw = __cov_$gxSu0pudM2bOKAjHXd1vw.__coverage__;
+if (!(__cov_$gxSu0pudM2bOKAjHXd1vw['server/config/local.env.sample.js'])) {
+   __cov_$gxSu0pudM2bOKAjHXd1vw['server/config/local.env.sample.js'] = {"path":"server/config/local.env.sample.js","s":{"1":0},"b":{},"f":{},"fnMap":{},"statementMap":{"1":{"start":{"line":8,"column":0},"end":{"line":14,"column":2}}},"branchMap":{}};
+}
+__cov_$gxSu0pudM2bOKAjHXd1vw = __cov_$gxSu0pudM2bOKAjHXd1vw['server/config/local.env.sample.js'];
+__cov_$gxSu0pudM2bOKAjHXd1vw.s['1']++;module.exports={DOMAIN:'http://localhost:9000',SESSION_SECRET:'pokerestimate-secret',DEBUG:''};
